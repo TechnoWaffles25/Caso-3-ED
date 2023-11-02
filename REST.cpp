b4c5471e208ef436c21bab4d9a05fabf263e52d3
Alejandro Montero <amontero0525@gmail.com>
2023-11-01 23:10:15 -0600

    reunion rodri

--- REST.cpp
+++ REST.cpp
@@ -3,6 +3,8 @@
 #include "cpp-httplib-master/httplib.h"
 using json = nlohmann::json;
 
+using namespace std;
+
 // Define la clase ChatGPT (asegúrate de definirla correctamente)
 class ChatGPT {
 public:
@@ -19,19 +21,22 @@ int main() {
 
     httplib::Server svr;
 
-    svr.Post("/api/convertirFrase", [&chatGPTInstance](const httplib::Request& req, httplib::Response& res) {
+    svr.Get("/api/convertirFrase", [&chatGPTInstance](const httplib::Request& req, httplib::Response& res) {
         // Obtiene el cuerpo de la solicitud POST (que debe contener la frase del usuario)
-        std::string userInput = req.get_param_value("frase");
+        /*std::string userInput = req.get_param_value("frase");
 
         // Realiza la conversión de la frase utilizando la instancia de ChatGPT
         std::string respuesta = chatGPTInstance.convertirFrase(userInput);
-
+        // Empieza la busqueda en el AVL
         // Crea una respuesta JSON
         json respuestaJson = {
             {"respuesta", respuesta}
+        };*/
+        json respuestaJson = {
+            {"respuesta", "Hola mundo"}
         };
-
         res.set_content(respuestaJson.dump(), "application/json");
+        cout << "Hola mundo" << endl;
     });
 
     std::cout << "Servidor REST en ejecución en http://localhost:8080/" << std::endl;
@@ -40,3 +45,5 @@ int main() {
 
     return 0;
 }  // Fin de main()
+
+// http://localhost:8080/api/convertirFrase?frase=Hola
