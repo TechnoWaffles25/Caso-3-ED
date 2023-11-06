9751a8e830453634cf3419fbd89afe3155fb7c4d
Luis <lpoveda@estudiantec.cr>
2023-11-06 17:49:06 -0600

    xd

--- Makefile
+++ Makefile
@@ -5,3 +5,9 @@ g++ C:\MinGW\include\winsock2.h -o main.o main.cpp
 
 g++ -o REST.o REST.h
 ./REST.o
+
+docker run -it --rm -v "C:\Users\cococo\3erCasoED:/home" gcc bash
+
+g++ -o REST.exe REST.cpp
+
+./REST.o
--- REST.cpp
+++ REST.cpp
@@ -2,48 +2,34 @@
 #include "json.hpp"
 #include "cpp-httplib-master/httplib.h"
 using json = nlohmann::json;
-
 using namespace std;
-
-// Define la clase ChatGPT (asegúrate de definirla correctamente)
 class ChatGPT {
 public:
     std::string convertirFrase(const std::string& userInput) {
-        // Implementa la lógica de conversión aquí
-        // Esta es solo una implementación de ejemplo
         return "Respuesta a: " + userInput;
     }
 };
-
 int main() {
-    // Crea una instancia de la clase ChatGPT
     ChatGPT chatGPTInstance;
-
     httplib::Server svr;
-
-    svr.Get("/api/convertirFrase", [&chatGPTInstance](const httplib::Request& req, httplib::Response& res) {
-        // Obtiene el cuerpo de la solicitud POST (que debe contener la frase del usuario)
-        /*std::string userInput = req.get_param_value("frase");
-
-        // Realiza la conversión de la frase utilizando la instancia de ChatGPT
+    svr.Post("/api/convertirFrase", [&chatGPTInstance](const httplib::Request& req, httplib::Response& res) {
+        /* std::string userInput = req.body;
         std::string respuesta = chatGPTInstance.convertirFrase(userInput);
-        // Empieza la busqueda en el AVL
-        // Crea una respuesta JSON
         json respuestaJson = {
             {"respuesta", respuesta}
-        };*/
+        };
+        res.set_header("Content-Type", "application/json");
+        res.status = 200;
+        res.set_content(respuestaJson.dump(), "application/json");
+    });*/
         json respuestaJson = {
             {"respuesta", "Hola mundo"}
         };
         res.set_content(respuestaJson.dump(), "application/json");
         cout << "Hola mundo" << endl;
     });
-
     std::cout << "Servidor REST en ejecución en http://localhost:8080/" << std::endl;
-
-    svr.listen("localhost", 8085);
-
+    svr.listen("localhost", 8080);
     return 0;
-}  // Fin de main()
-
+}
 // http://localhost:8080/api/convertirFrase?frase=Hola
